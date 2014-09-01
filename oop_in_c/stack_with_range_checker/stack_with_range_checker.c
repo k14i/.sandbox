82486fd3b660796ce4b394c2c6a07acda1208718
Keisuke TAKAHASHI <keithseahus@gmail.com>
2014-09-02 01:26:56 +0900

    Update oop_in_c.

--- oop_in_c/stack_with_range_checker/stack_with_range_checker.c
+++ oop_in_c/stack_with_range_checker/stack_with_range_checker.c
@@ -1,9 +1,46 @@
+#include <stdbool.h>
+#include "stack.h"
+
+static bool isStackFull(const Stack *p) {
+  return p->top == p->size;
+}
+
+static bool isStackEmpty(const Stack *p) {
+  return p->top == 0;
+}
+
+bool validateRange(Validator *pThis, int val) {
+  Range *pRange = (Range *)(pThis->pData);
+  return pRange->min <= val && val <= pRange->max;
+}
+
+bool validatePrevious(Validator *pThis, int val) {
+  PreviousValue *pPrevious = (PreviousValue *)pThis->pData;
+  if(val < pPrevious->previousValue) return false;
+  pPrevious->previousValue = val;
+  return true;
+}
+
+/*
 static bool isRangeOk(const Stack *p, int val) {
   return !p->needRangeCheck || (p->min <= val && val <= p->max);
 }
+*/
+
+bool validate(Validator *p, int val) {
+  if(!p) return true;
+  return p->validate(p, val);
+}
 
 bool push(Stack *p, int val) {
-  if(!isRangeOk(p->Range, val) || isStackFull(p)) return false;
+  //if(!isRangeOk(p->Range, val) || isStackFull(p)) return false;
+  if(!validate(p->pValidator, val) || isStackFull(p)) return false;
   p->pBuf[p->top++] = val;
+  return true;
+}
+
+bool pop(Stack *p, int *pRet) {
+  if(isStackEmpty(p)) return false;
+  *pRet = p->pBuf[--p->top];
   return true;
 }
--- oop_in_c/stack_with_range_checker/stack_with_range_checker.h
+++ oop_in_c/stack_with_range_checker/stack_with_range_checker.h
@@ -1,14 +1,36 @@
+#ifndef _STACK_H_
+#define _STACK_H_
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct Validator {
+  bool (*const validate)(struct Validator *pThis, int val);
+  void *const pData;
+} Validator;
+
 typedef struct {
   const int min;
   const int max;
 } Range;
 
 typedef struct {
+  int previousValue;
+} PreviousValue;
+
+typedef struct {
   int top;
   const size_t size;
-  int * const pBuf;
-  const Range *const pRange;
+  int *const pBuf;
+  //const Range *const pRange;
+  Validator *const pValidator;
 } Stack;
+
+bool push(Stack *p, int val);
+bool pop(Stack *p, int *pRet);
 
 #define newStack(buf) {                         \
     0, sizeof(buf) / sizeof(int), (buf),        \
@@ -19,3 +41,24 @@ typedef struct {
     0, sizeof(buf) / sizeof(int), (buf),        \
       pRange                                    \
 }
+
+#define newStackWithValidator(buf, pValidator) {  \
+    0, sizeof(buf) / sizeof(int), (buf),          \
+      pValidator                                  \
+}
+
+#define rangeValidator(pRange) {                \
+    validateRange,                              \
+      pRange                                    \
+}
+
+#define previousValidator(pPrevious) {          \
+    validatePrevious,                           \
+      pPrevious                                 \
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
