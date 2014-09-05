ab3b2a4cc6d9f0012a34cbe8d97cd77165ddc6e4
Keisuke TAKAHASHI <keithseahus@gmail.com>
2014-09-06 02:11:44 +0900

    WIP

--- list_in_c/list.c
+++ list_in_c/list.c
@@ -1,57 +1,110 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "list.h"
 
-static int List_terminate(List *self) {
-  List list = newList();
-  if(self->next) return -1;
-  self->next = &list;
-  return 0;
-}
-
-static void List_append(List *self, List *list) {
+static void List_append(List *self, List *target) {
+  printf("---- %s ----\n", __func__);
   List *ptr;
   
   ptr = self;
-  while (ptr) {
-    if(!ptr->next) break;
-    ptr = ptr->next;
+  for (int i=0;;i++) {
+	printf("i = %d\n", i);
+	printf("ptr(%p)->next(%p)\n", ptr, ptr->next);
+	if(ptr->next) {
+	  printf("ptr(%p)->next(%p) is not NULL\n", ptr, ptr->next);
+	  printf("ptr->data is %s\n", ptr->data);
+	  ptr = ptr->next;
+	  printf("ptr->data is %s\n", ptr->data);
+	}
+	printf("Append target(%p) to ptr->next(%p)\n", target, ptr->next);
+    ptr->next = target;
+    printf("Now, ptr->next is %p\n", ptr->next);
+	break;
   }
+
+  return;
+}
+
+static int List_terminate(List *self) {
+  printf("---- %s ----\n", __func__);
+  List null_list = newList();
+  printf("Terminate %p\n", self);
+  self->append(self, &null_list);
+  return 0;
+}
+
+static void List_parse(List *self) {
+  printf("---- %s ----\n", __func__);
+  List *ptr;
+
+  ptr = self;
+  for (int i=0;;i++) {
+	printf("i = %d\n", i);
+    printf("ptr->data = %s\n", ptr->data);
+    printf("ptr->next = %p\n", ptr->next);
+    if(!ptr->next) break;
+    printf("ptr->data = %s\n", ptr->data);
+    ptr = ptr->next;
+    printf("ptr->data = %s\n", ptr->data);
+  }
+
+  return;
 }
 
 int main(int argc, char *argv[]) {
-  char foo[256];
-  char bar[256];
-  List *ptr;
+  void *buf;
+  char strbuf[256];
 
-  sprintf(foo, "foo");
-  List list1 = newList();
-  list1.data = &foo;
-  //printf("%s\n", list1.data);
+  //void *list = malloc(sizeof(List));
+  //memset(list, 0, sizeof(list));
+  //(List *)list = newList();
+  List list = newList();
+
+  // 1
+  char *strbuf1 = malloc(sizeof(strbuf));
+  memset(strbuf1, 0, sizeof(strbuf1));
+  sprintf(strbuf1, "foo");
   
-  sprintf(bar, "bar");
-  List list2 = newList();
-  list2.data = &bar;
-  //printf("%s\n", list2.data);
-  
-  list1.next = &list2;
-  //memcpy(ptr, &list1, sizeof(&list1));
-  ptr = &list1;
-  
-  List list3 = newList();
-  list2.next = &list3;
-  
-  //printf("%p, %p\n", ptr, &list1);
-  printf("%p\n", ptr->next);
-  
-  for (int i=0;;i++) {
-    printf("%p\n", ptr);
-    if(!ptr->next) break;
-    printf("%s\n", ptr->data);
-    ptr = ptr->next;
-  }
-  
+  List *list1 = malloc(sizeof(list));
+  //memset(list1, 0, sizeof(list1));
+  memcpy(list1, &list, sizeof(list));
+
+  //list1->data = &strbuf;
+  //sprintf(list1->data, strbuf);
+  memcpy(list1->data, strbuf1, sizeof(strbuf1));
+  printf("list1(%p)->data(%p) = %s\n", list1, list1->data, list1->data);
+  //
+
+/*
+  // 2
+  memset(strbuf, 0, sizeof(strbuf));
+  sprintf(strbuf, "bar");
+  List *list2 = malloc(sizeof(list));
+  memcpy(list2, &list, sizeof(list));
+
+  //list2->data = &strbuf;
+  sprintf(list2->data, strbuf);
+  printf("list1(%p)->data(%p) = %s\n", list1, list1->data, list1->data);
+  printf("list2(%p)->data(%p) = %s\n", list2, list2->data, list2->data);
+  //
+
+  list1->append(list1, list2);
+*/
+
+/*
+  if(list1->terminate(&list1) != 0) goto err;
+  printf("list1->data = %s\n", list1->data);
+
+  list1->parse(list1);
+*/
+
+  free(list1);
   return 0;
+
+err:
+  free(list1);
+  return -1;
 }
 
--- list_in_c/list.h
+++ list_in_c/list.h
@@ -1,20 +1,24 @@
 typedef int (*Member_int_0)(void*);
 typedef void (*Member_void_1_List)(void*, void*);
+typedef void (*Member_void_0)(void*);
 
 typedef struct
 {
   void *data;
   void *next;
-  Member_int_0 terminate;
   Member_void_1_List append;
+  Member_int_0 terminate;
+  Member_void_0 parse;
 } List;
 
 #define newList() { \
     NULL, NULL, \
+      (void*)&List_append,    \
       (void*)&List_terminate, \
-      (void*)&List_append \
+      (void*)&List_parse,     \
 }
 
-static int List_terminate(List *self);
 static void List_append(List *self, List *list);
+static int List_terminate(List *self);
+static void List_parse(List *self);
 
