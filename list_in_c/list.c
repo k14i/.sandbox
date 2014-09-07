84bf1a1e62e52493cd65ae86050eba6f1954feaf
Keisuke TAKAHASHI <keithseahus@gmail.com>
2014-09-07 23:39:07 +0900

    Complete.

--- list_in_c/list.c
+++ list_in_c/list.c
@@ -4,52 +4,70 @@
 
 #include "list.h"
 
-static void List_append(List *self, List *target) {
-  printf("---- %s ----\n", __func__);
-  List *ptr;
-  
-  ptr = self;
+static void List_append(List *ptr, List *target) {
+  printf("\n/* ---- %s ----\n", __func__);
+  //List *ptr;
+
+  printf("target(%p)->data(%p) is %s\n", target, target->data, target->data);
+
+  //ptr = self;
   for (int i=0;;i++) {
 	printf("i = %d\n", i);
 	printf("ptr(%p)->next(%p)\n", ptr, ptr->next);
-	if(ptr->next) {
-	  printf("ptr(%p)->next(%p) is not NULL\n", ptr, ptr->next);
+	while(ptr->next) {
+	  printf("... ptr(%p)->next(%p) is not NULL. Go ahead.\n", ptr, ptr->next);
 	  printf("ptr->data is %s\n", ptr->data);
 	  ptr = ptr->next;
 	  printf("ptr->data is %s\n", ptr->data);
 	}
-	printf("Append target(%p) to ptr->next(%p)\n", target, ptr->next);
-    ptr->next = target;
-    printf("Now, ptr->next is %p\n", ptr->next);
+	printf("... ptr(%p)->next(%p) is NULL. Append target(%p) to ptr->next(%p)\n", ptr, ptr->next, target, ptr->next);
+	printf("target(%p)->data(%p) is %s\n", target, target->data, target->data);
+	ptr->next = malloc(sizeof(void*));
+	memcpy(ptr->next, target, sizeof(void*));
+    //ptr->next = target;
+    printf("Now, ptr->next has become %p\n", ptr->next);
+    ptr = ptr->next;
+    printf("ptr->next(%p)->data(%p) is %s\n", ptr, ptr->data, ptr->data);
 	break;
   }
 
+  printf("---- %s ---- */\n", __func__);
   return;
 }
 
 static int List_terminate(List *self) {
-  printf("---- %s ----\n", __func__);
+  printf("\n/* ---- %s ----\n", __func__);
   List null_list = newList();
-  printf("Terminate %p\n", self);
+  printf("Terminate %p with null_list(%p)\n", self, &null_list);
   self->append(self, &null_list);
+
+  printf("---- %s ---- */\n", __func__);
   return 0;
 }
 
 static void List_parse(List *self) {
-  printf("---- %s ----\n", __func__);
+  printf("\n/* ---- %s ----\n", __func__);
   List *ptr;
 
   ptr = self;
   for (int i=0;;i++) {
 	printf("i = %d\n", i);
-    printf("ptr->data = %s\n", ptr->data);
-    printf("ptr->next = %p\n", ptr->next);
-    if(!ptr->next) break;
-    printf("ptr->data = %s\n", ptr->data);
+    printf("ptr(%p)->data(%p) = %s\n", ptr, ptr->data, ptr->data);
+    printf("ptr(%p)->next(%p) = %p\n", ptr, ptr->next, ptr->next);
+    if(!ptr->next) {
+	  printf("... ptr(%p)->next(%p) is NULL. break.\n", ptr, ptr->next);
+      break;
+	};
+	printf("... ptr(%p)->next(%p) is not NULL. continue.\n", ptr, ptr->next);
     ptr = ptr->next;
-    printf("ptr->data = %s\n", ptr->data);
   }
 
+  printf("---- %s ---- */\n", __func__);
+  return;
+}
+
+static void List_initialize(List *self) {
+  initList(self);
   return;
 }
 
@@ -65,27 +83,34 @@ int main(int argc, char *argv[]) {
 
   // 1
   List list1 = newList();
+  list1.initialize(&list1);
   sprintf(strbuf, "foo");
   memcpy(list1.data, strbuf, sizeof(strbuf));
 
-  printf("list1(%p)->data(%p) = %s\n", &list1, &(list1.data), list1.data);
-
   // 2
   List list2 = newList();
+  list2.initialize(&list2);
   sprintf(strbuf, "bar");
   memcpy(list2.data, strbuf, sizeof(strbuf));
 
-  printf("list1(%p).data(%p) = %s\n", &list1, &(list1.data), list1.data);
-  printf("list2(%p).data(%p) = %s\n", &list2, &(list2.data), list2.data);
+  printf("list1(%p).data(%p) is %s\n", &list1, list1.data, list1.data);
+  printf("list1(%p).next is %p\n", &list1, list1.next);
+  printf("list2(%p).data(%p) is %s\n", &list2, list2.data, list2.data);
+  printf("list2(%p).next is %p\n", &list2, list2.next);
 
-  //list1->append(list1, list2);
+  list1.append(&list1, &list2);
+  printf("list1(%p).data(%p) is %s\n", &list1, list1.data, list1.data);
+  printf("list1(%p).next is %p\n", &list1, list1.next);
+  printf("list2(%p).data(%p) is %s\n", &list2, list2.data, list2.data);
+  printf("list2(%p).next is %p\n", &list2, list2.next);
 
-/*
-  if(list1->terminate(&list1) != 0) goto err;
-  printf("list1->data = %s\n", list1->data);
+  if(list1.terminate(&list1) != 0) goto err;
+  printf("list1(%p).data(%p) is %s\n", &list1, list1.data, list1.data);
+  printf("list1(%p).next is %p\n", &list1, list1.next);
+  printf("list2(%p).data(%p) is %s\n", &list2, list2.data, list2.data);
+  printf("list2(%p).next is %p\n", &list2, list2.next);
 
-  list1->parse(list1);
-*/
+  list1.parse(&list1);
 
   exit_status = EXIT_SUCCESS;
   goto clean_up_and_exit;
@@ -97,6 +122,7 @@ err:
 clean_up_and_exit:
   list1.destroy(&list1);
   list2.destroy(&list2);
+  printf("Exit with exit_status(%d)\n", exit_status);
   return exit_status;
 }
 
--- list_in_c/list.h
+++ list_in_c/list.h
@@ -9,24 +9,32 @@ typedef struct
   Member_void_1_List append;
   Member_int_0 terminate;
   Member_void_0 parse;
+  Member_void_0 initialize;
   Member_void_0 destroy;
 } List;
 
-#define newList() { \
-    malloc(sizeof(void*)),    \
-      malloc(sizeof(void*)),  \
-      (void*)&List_append,    \
-      (void*)&List_terminate, \
-      (void*)&List_parse,     \
-      (void*)&List_destroy,   \
+#define newList() {                             \
+    NULL,                                       \
+      NULL,                                     \
+      (void*)&List_append,                      \
+      (void*)&List_terminate,                   \
+      (void*)&List_parse,                       \
+      (void*)&List_initialize,                  \
+      (void*)&List_destroy,                     \
 }
 
-#define destroyList(self) { \
-    free(self->data);       \
-    free(self->next);       \
+#define initList(self) {                        \
+    self->data = malloc(sizeof(void*));         \
+    memset(self->data, 0, sizeof(self->data));  \
+}
+
+#define destroyList(self) {                     \
+    if(self->data) free(self->data);            \
+    if(self->next) free(self->next);            \
 }
 
 static void List_append(List *self, List *list);
 static int List_terminate(List *self);
 static void List_parse(List *self);
+static void List_initialize(List *self);
 static void List_destroy(List *self);
