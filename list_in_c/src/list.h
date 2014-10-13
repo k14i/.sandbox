96da48488323275eac4b51714ad8703b44a36d50
Keisuke TAKAHASHI <keithseahus@gmail.com>
2014-10-14 08:03:23 +0900

    Add ListHelper Object and fix some indents.

--- list_in_c/src/list.h
+++ list_in_c/src/list.h
@@ -1,52 +1,51 @@
+#ifndef LIST_H
+#define LIST_H
+
 //typedef struct {} List;
+
+/*
+ * List Object
+ */
+
 typedef int (*Member_int_0)(void*);
 typedef void (*Member_void_1_List)(void*, void*);
 typedef void (*Member_void_0)(void*);
 typedef void (*Member_void_1_void)(void*, void*);
 typedef void (*Member_void_1_int)(void*, int);
 typedef void (*Member_void_2_void_int)(void*, void*, int);
-typedef void (*Member_List_1_int)(void*, int);
-typedef void (*Member_List_0)(void*);
-//typedef int (*Member_List_0)(void*);
-
-typedef void (*Func)(void*, void*);
 
 typedef struct
 {
-  void                   *data;
-  void                   *next;
-  void                   *prev;
-  int                    tag;
-  Member_void_1_List     append;
-  Member_void_1_List     add;
-  Member_void_1_int      set_tag;
-  Member_void_1_int      add_tag;
-  Member_void_2_void_int add_with_tag;
-  Member_int_0           terminate;
-  Member_void_1_List     dump;
-  Member_List_1_int      find_by_tag;
-  Member_void_1_void     foreach;
-  Member_List_0          last;
-  Member_void_1_void     reverse;
-  Member_void_0          initialize;
-  Member_void_0          destroy;
+	void                   *data;
+	void                   *next;
+	void                   *prev;
+	int                    tag;
+	Member_void_1_List     append;
+	Member_void_1_List     add;
+	Member_void_1_int      set_tag;
+	Member_void_1_int      add_tag;
+	Member_void_2_void_int add_with_tag;
+	Member_int_0           terminate;
+	Member_void_1_List     dump;
+	Member_void_1_void     foreach;
+	Member_void_1_void     reverse;
+	Member_void_0          initialize;
+	Member_void_0          destroy;
 } List;
 
 #define newList() {                             \
-    NULL, NULL, NULL, 0,                        \
-      (void*)&List_append,                      \
-      (void*)&List_add,                         \
-      (void*)&List_set_tag,                     \
-      (void*)&List_add_tag,                     \
-      (void*)&List_add_with_tag,                \
-      (void*)&List_terminate,                   \
-      (void*)&List_dump,                        \
-      (void*)&List_find_by_tag,                 \
-      (void*)&List_foreach,                     \
-      (void*)&List_last,                        \
-      (void*)&List_reverse,                     \
-      (void*)&List_initialize,                  \
-      (void*)&List_destroy,                     \
+		NULL, NULL, NULL, 0,					\
+		(void*)&List_append,					\
+			(void*)&List_add,					\
+			(void*)&List_set_tag,				\
+			(void*)&List_add_tag,				\
+			(void*)&List_add_with_tag,			\
+			(void*)&List_terminate,				\
+			(void*)&List_dump,					\
+			(void*)&List_foreach,				\
+			(void*)&List_reverse,				\
+			(void*)&List_initialize,			\
+			(void*)&List_destroy,				\
 }
 
 #define initializeList(self) {                  \
@@ -66,10 +65,35 @@ static void List_add_tag(List *self, int tag);
 static void List_add_with_tag(List *self, void *target, int tag);
 static int List_terminate(List *self);
 static void List_dump(List *self, List *list);
-static List *List_find_by_tag(List *self, int tag);
 static void List_foreach(List *self, void *fun);
-//static void *List_last(List *self);
-static List *List_last(List *self);
 static void List_reverse(List *self, void *fun);
 static void List_initialize(List *self);
 static void List_destroy(List *self);
+
+
+/*
+ * ListHelper Object
+ */
+
+typedef List *(*Member_List_1_int)(void*, int);
+typedef List *(*Member_List_0)(void*);
+//typedef int (*Member_List_0)(void*);
+
+typedef struct
+{
+	Member_List_1_int  find_by_tag;
+	Member_List_0      last;
+} ListHelper;
+
+#define newListHelper() {						\
+		(void*)&List_find_by_tag,				\
+			(void*)&List_last,					\
+}
+
+static List *List_find_by_tag(List *self, int tag);
+static List *List_last(List *self);
+
+#endif
+
+
+typedef void (*Func)(void*, void*);
