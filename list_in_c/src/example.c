91e6b3f61a1021caa1b861cde173f27d8b0a0fb3
Keisuke TAKAHASHI <keithseahus@gmail.com>
2014-10-15 06:29:49 +0900

    Update directory structures and separate main() from list.c

---
+++ list_in_c/src/example.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "lib/list.h"
+
+int main(int argc, char *argv[]) {
+  int exit_status = 0;
+  void *buf;
+  char strbuf[256];
+
+  // 1
+  List list1 = newList();
+  list1.initialize(&list1);
+  sprintf(strbuf, "foo");
+  memcpy(list1.data, strbuf, sizeof(strbuf));
+
+  // 2
+  List list2 = newList();
+  list2.initialize(&list2);
+  sprintf(strbuf, "bar");
+  memcpy(list2.data, strbuf, sizeof(strbuf));
+
+  list1.foreach(&list1, &(list1.dump));
+
+  list1.append(&list1, &list2);
+  list1.foreach(&list1, &(list1.dump));
+
+  if(list1.terminate(&list1) != 0) goto err;
+
+  //list1.foreach(&list1, &(list1.dump));
+  list1.reverse(&list1, &(list1.dump));
+  //List *ptr;
+  //list1.last(&list1);
+  //memcpy(ptr, list1.last(&list1), sizeof(List*));
+
+  exit_status = EXIT_SUCCESS;
+  goto clean_up_and_exit;
+
+err:
+  exit_status = EXIT_FAILURE;
+  goto clean_up_and_exit;
+
+clean_up_and_exit:
+  list1.destroy(&list1);
+  list2.destroy(&list2);
+  printf("Exit with exit_status(%d)\n", exit_status);
+  return exit_status;
+}
+
