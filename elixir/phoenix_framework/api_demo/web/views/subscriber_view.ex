499735580b34cd00ae416aeac1de184f3e16c3aa
Keisuke Takahashi <keithseahus@gmail.com>
2015-06-21 08:35:02 +0900

    Make subscribers formatted

--- elixir/phoenix_framework/api_demo/web/views/subscriber_view.ex
+++ elixir/phoenix_framework/api_demo/web/views/subscriber_view.ex
@@ -1,12 +1,30 @@
 defmodule ApiDemo.Api.V1.SubscriberView do
   use ApiDemo.Web, :view
 
-  # def render("index.json", %{subscribers: subscribers}) do
-  #   subscribers
-  # end
+  def render("index.json", %{subscribers: subscribers}) do
+    #subscribers
+    format(%{subscribers: subscribers})
+  end
 
   def render("show.json", %{subscriber: subscriber}) do
     format(%{subscriber: subscriber})
+  end
+
+  defp format_subscribers([h|t], acc) do
+    subscriber = %{
+      phone_number: h.phone_number,
+      created_at: h.inserted_at,
+      updated_at: h.updated_at,
+    }
+    format_subscribers(t, acc ++ [subscriber])
+  end
+
+  defp format_subscribers([], acc) do
+    acc
+  end
+
+  defp format(%{subscribers: subscribers}) do
+    format_subscribers(subscribers, [])
   end
 
   defp format(%{subscriber: subscriber}) do
