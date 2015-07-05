b0690462ad61431e4c4e9c08eeb08cc57618374f
Keisuke Takahashi <keithseahus@gmail.com>
2015-07-05 23:26:52 +0900

    Update versions

--- elixir/phoenix_framework/api_demo/mix.exs
+++ elixir/phoenix_framework/api_demo/mix.exs
@@ -34,6 +34,8 @@ defmodule ApiDemo.Mixfile do
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 1.0"},
      {:phoenix_live_reload, "~> 0.4", only: :dev},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:ecto, "~> 0.12.0"},
+    ]
   end
 end
